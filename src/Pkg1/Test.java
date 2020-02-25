ad481da734c87625ca5d70ebf9cb28aff617a7d1
suchita <suchitamulik69@gmail.com>
2020-02-25 19:12:30 +0530

    Hi

--- src/Pkg1/Test.java
+++ src/Pkg1/Test.java
@@ -1,7 +1,9 @@
 package Pkg1;
 
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		int i=10;
 		System.out.println("Hello world");
 		System.out.println("Hello");
 	}
